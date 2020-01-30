23f5bdfda6f64db6fb764e83649c649e774affba
Robert Scheibe <rob.scheibe@gmail.com>
2020-01-30 14:30:16 +0100

    changed extra pause key from S to @

--- README.txt
+++ README.txt
@@ -1,2 +1,3 @@
-./send_keys.sh assdSg | xargs xte
+# @ adds an extra pause
+./send_keys.sh assd@g | xargs xte
 
--- send_keys.sh
+++ send_keys.sh
@@ -1,17 +1,16 @@
 #/bin/bash
-#keys are always small caps
-#S is 500.000us sleep between keypresses
-#standard sleep between keypresses are 100.000us
+# @ is 500.000us sleep between keypresses
+# standard sleep between keypresses are 75000us
 
 S=75000
-ES=500000
+ExtendedSleep=500000
 #keys="fdSfd"
 keys=$1
 output=""
 
 for (( i=0; i<${#keys}; i++ )); do
-    if [ ${keys:$i:1} == "S" ]; then
-        output="$output 'usleep $ES'"
+    if [ ${keys:$i:1} == "@" ]; then
+        output="$output 'usleep $ExtendedSleep'"
     else
         output="$output 'key ${keys:$i:1}' 'usleep $S'"
     fi
